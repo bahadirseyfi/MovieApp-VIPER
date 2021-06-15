c937f83991bc53b14cc198fca94bafeee05456aa
bahadirseyfi <bahadirsoftware@gmail.com>
2021-06-16 01:56:02 +0300

    Added - Populer Movies Cell's Presenter(NOT ACTIVATED)

---
+++ MovieApp/MovieApp/Module/Movie/MovieCollectionCell/PopularMovieCollectionCellPresenter.swift
@@ -0,0 +1,30 @@
+//
+//  PopularMovieCollectionCellPresenter.swift
+//  MovieApp
+//
+//  Created by bahadir on 16.06.2021.
+//
+
+/*
+import Foundation
+
+protocol PopularMovieCollectionCellPresenterInterface {
+    func load()
+}
+
+final class PopularMovieCollectionCellPresenter {
+    weak var view: PopularMovieCellInterface?
+    private let movie: Movie
+    init(view: PopularMovieCellInterface,
+         movie: Movie) {
+        self.view = view
+        self.movie = movie
+    }
+}
+
+extension PopularMovieCollectionCellPresenter: PopularMovieCollectionCellPresenterInterface {
+    func load() {
+        
+    }
+}
+*/
--- MovieApp/MovieApp/Module/Movie/MovieViewController.swift
+++ MovieApp/MovieApp/Module/Movie/MovieViewController.swift
@@ -11,16 +11,16 @@ import UIKit
 protocol MovieViewInterface: AnyObject{
     func reloadPopular()
     func prepareCollectionViews()
-  //  func showToLoading()
-  //  func hideToLoading()
+    //  func showToLoading()
+    //  func hideToLoading()
 }
 
 final class MovieViewController: UIViewController {
-
+    
     @IBOutlet private weak var popularCollectionView: UICollectionView!
     
     var presenter: MoviePresenterInterface!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter.viewDidLoad()
@@ -41,35 +41,37 @@ final class MovieViewController: UIViewController {
 }
 
 extension MovieViewController: UICollectionViewDataSource, UICollectionViewDelegate {
-
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return presenter.numberOfItems() ?? 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-            let cell = popularCollectionView.dequeCell(cellType: PopularMovieCell.self, indexPath: indexPath)
-            if let movie = presenter.popularMovie(indexPath.item) {
-                let movieImage =  MovieDBBaseAPI.BaseImagePath + movie.backDropPath
-                cell.configureCell(image: movieImage)
-            }
-            return cell
+        let cell = popularCollectionView.dequeCell(cellType: PopularMovieCell.self, indexPath: indexPath)
+        if let movie = presenter.popularMovie(indexPath.item) {
+            let movieImage =  MovieDBBaseAPI.BaseImagePath + movie.backDropPath
+            cell.configureCell(image: movieImage)
+ //           cell.presenter = PopularMovieCollectionCellPresenter(view: cell as! PopularMovieCellInterface,
+   //                                                              movie: movie)
+        }
+        return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-            guard let movieID = presenter.popularMovie(indexPath.item)?.movieId else {
-                print("Başarız ID ")
-                return
-            }
-            redirectTo(movieID: movieID)
+        guard let movieID = presenter.popularMovie(indexPath.item)?.movieId else {
+            print("Başarız ID ")
+            return
         }
+        redirectTo(movieID: movieID)
+    }
 }
 
 extension MovieViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let size = presenter.calculateCellSize(collectionViewWidth: Double(collectionView.frame.size.width))
-             return .init(width: size.width, height: size.height)
+        return .init(width: size.width, height: size.height)
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         .init(top: .zero, left: CGFloat(presenter.cellPadding), bottom: .zero, right: CGFloat(presenter.cellPadding))
     }
